3172d2c69af2e0d9c87af183c17bd2d5ef02a935
Stephen Stucky <sstucky3@gmail.com>
2015-01-31 14:26:18 -0600

    fuck crafty

---
+++ js/index.js
@@ -0,0 +1,10 @@
+Game = {
+    
+    start: function() {
+        Crafty.init(window.innerWidth, window.innerHeight);
+        Crafty.canvas.init();
+        
+        Crafty.background("url('assessts/background.png')");
+    
+    
+}
